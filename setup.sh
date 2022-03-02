ff8dbd59d0a99401260632d186c86d267325ce58
Ashok Kumar Selvam <akashokkumar300@gmail.com>
2022-03-02 16:33:47 -0500

    fix: path to requirements.txt

--- setup.sh
+++ setup.sh
@@ -6,14 +6,14 @@ if [ -z "$SPI" ]
 then
 	echo "SPI is not enabled. Entry is added in /boot/config.txt. Reboot to enable(sudo reboot)"
 else
+	SRC_PATH=$(pwd)
 	sudo apt-get update -y
 	sudo apt-get upgrade -y
 	sudo apt-get install -y python3-dev python3-spidev
-	PWD=$(pwd)
 	cd ~
 	git clone https://github.com/Gadgetoid/py-spidev.git
 	cd py-spidev
 	sudo python setup.py install
-	cd $PWD
+	cd $SRC_PATH
 	pip install -r requirements.txt
 fi
