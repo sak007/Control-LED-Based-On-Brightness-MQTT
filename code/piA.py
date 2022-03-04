6db0cac5d0cde82fe4090ee32cb2a0bc51e3ebc5
Radek Pudelko <radekpudelko4@gmail.com>
2022-03-04 16:55:10 -0500

    adc raw reading, scaling, and scaled reading decoupled buttons from client, buttons are debounced piA main loop, added buttons to piB too

--- code/piA.py
+++ code/piA.py
@@ -2,10 +2,27 @@ from client import Client
 import json
 import time
 import RPi.GPIO as GPIO
-import PiStatus
-from adc import ADC
+import mygpio
+from adc import *
+import wifi
+
+PERCENT_DELTA = .05 # Publish changes of this %
+SAMPLE_TIME = .1 # Sample ADC every x seconds
 
 class PiAClient(Client):
+    def mysetup(self):
+        self.setStatusWill("status/RaspberryPiA")
+        self.setOnConnectMessage('status/RaspberryPiA', 'online')
+        self.setOnGracefulDisconnectMessage('status/RaspberryPiA', 'offline')
+
+        self.connect()
+
+        self.publishedLightSensor = None
+        self.publishedThreshold = None
+
+        self.subscribe("lightSensor")
+        self.subscribe("threshold")
+
     def on_message(self, client, userdata, msg):
         topic = str(msg.topic)
         payload = str(msg.payload.decode("utf-8"))
@@ -15,57 +32,106 @@ class PiAClient(Client):
         elif topic == 'threshold':
             self.publishedThreshold = payload
 
-        print ("Received Topic: " + topic + ", Value: " + payload)
-
-def updateLightSensor(client, newValue, deltaThreshold):
-    if client.publishedLightSensor == None:
-        client.publish("lightSensor", payload=str(newValue))
-        client.publishedLightSensor = str(newValue)
-    elif abs(float(newValue) - float(client.publishedLightSensor)) > deltaThreshold:
-        client.publish("lightSensor", payload=str(newValue))
-        client.publishedLightSensor = str(newValue)
-
-def updateThreshold(client, newValue, deltaThreshold):
-    if client.publishedThreshold == None:
-        client.publish("threshold", payload=str(newValue))
-        client.publishedThreshold = str(newValue)
-    elif abs(float(newValue) - float(client.publishedThreshold)) > deltaThreshold:
-        client.publish("threshold", payload=str(newValue))
-        client.publishedThreshold = str(newValue)
+        print("Received Topic: " + topic + ", Value: " + payload)
 
 
-if __name__ == "__main__":
+    def updateLightSensor(self, newValue, deltaThreshold):
+        # First Reading/Publish ?
+        if self.publishedLightSensor == None:
+            self.publish("lightSensor", payload=str(newValue))
+            self.publishedLightSensor = newValue
+        # Is % diff of old and new value > the deltaThreshold %
+        elif abs(newValue - float(self.publishedLightSensor)) > deltaThreshold:
+            self.publish("lightSensor", payload=str(newValue))
+            self.publishedLightSensor = newValue
 
+    def updateThreshold(self, newValue, deltaThreshold):
+        # First Reading/Publish ?
+        if self.publishedThreshold == None:
+            self.publish("threshold", payload=str(newValue))
+            self.publishedThreshold = newValue
+        # Is % diff of old and new value > the deltaThreshold % ?
+        elif abs(newValue - float(self.publishedThreshold)) > deltaThreshold:
+            self.publish("threshold", payload=str(newValue))
+            self.publishedThreshold = newValue
+
+# Sets up ADC, buttons, optional LEDs, and MQTT client
+# In a loop, reads from ADC every .1 s, if the client is running, then
+# an update to the last published values will be made if the PercentDelta is 
+# great enough, then the wifi and connection button are checked to see if they are
+# pressed and are handled
+# Note, after wifi is reconnected, if the client was not explicitly disconnected,
+# then it can be used without needed to reconnect/press the conn button
+def main():
+    # Load Connection Properties
     f = open('../properties.json')
     properties = json.load(f)
     BROKER_ADDR = properties['BROKER_ADDR']
     BROKER_PORT = properties['BROKER_PORT']
-    adc = ADC()
+    # Connect ADC
+    myadc = ADC()
+    # Setup GPIO
+    mygpio.setup()
+    wifiBtn = mygpio.Button(mygpio.WIFI_PIN)
+    connBtn = mygpio.Button(mygpio.CONN_PIN)
     try:
+        # Setup MQTT Client
         client = PiAClient(BROKER_ADDR, BROKER_PORT, 'RaspberryPiA')
-        client.setStatusWill("status/RaspberryPiA")
-        client.setOnConnectMessage('status/RaspberryPiA', 'online')
-        client.setOnGracefulDisconnectMessage('status/RaspberryPiA', 'offline')
-        client.connect()
-        client.publishedLightSensor = None
-        client.publishedThreshold = None
-
-        client.subscribe("lightSensor")
-        client.subscribe("threshold")
+        client.mysetup()
+        clientRunning = True # Protects client from publishing when it doesnt have a connection
+        mygpio.turnOn(mygpio.CONN_LIGHT_PIN) # Optional Conn LED
+        mygpio.turnOn(mygpio.WIFI_LIGHT_PIN) # Optional Wifi LED
+        lastReadTime = time.time()
 
         while True:
-            ls = adc.read(0)
-            updateLightSensor(client, ls, 100)
-            th = adc.read(1)
-            updateThreshold(client, th, 100)
+            # Read from the ADC
+            if (time.time() - lastReadTime) >= SAMPLE_TIME:
+                ls = readLDR(myadc)
+                th = readPOT(myadc)
+                #print("Last Read Time %f seconds" % (time.time() - lastReadTime))
+                lastReadTime = time.time()
 
-            PiStatus.setupWifiButton(client)
-            PiStatus.setupConnButton(client)
+                # Update values if client is running safely
+                if clientRunning:
+                    client.updateLightSensor(ls, PERCENT_DELTA)
+                    client.updateThreshold(th, PERCENT_DELTA)
 
-            # light = getLightValue()
-            # client.publish("lightSensor", payload='1')
-            # client.publish("threshold", payload='1')
+            # Check if the button was pressed
+            if wifiBtn.checkState() == mygpio.BTN_PRESS:
+                if wifi.isWifiEnabled(): # Disable Wifi
+                    wifi.disableWifi()
+                    mygpio.turnOff(mygpio.WIFI_LIGHT_PIN) # Optional Wifi LED
+                    mygpio.turnOff(mygpio.CONN_LIGHT_PIN) # Optional Conn LED
+                    clientRunning = False
+                else: # Enable Wifi
+                    wifi.enableWifi() 
+                    mygpio.turnOn(mygpio.WIFI_LIGHT_PIN) # Optional Wifi LED    
+                    if client.isConnected(): # Was the client connected before Wifi was shutdown
+                        mygpio.turnOn(mygpio.CONN_LIGHT_PIN) # Optional Wifi LED
+                        clientRunning = True      
+
+            # If wifi is enabled, check the state of the conn button
+            if wifi.isWifiEnabled():
+                # Check if the conn button was pressed
+                if connBtn.checkState() == mygpio.BTN_PRESS:
+                    if client.isConnected(): # Disconnect
+                        client.disconnect()
+                        mygpio.turnOff(mygpio.CONN_LIGHT_PIN) # Optional Conn LED
+                        clientRunning = False
+                    else: # Connect
+                        client.connect()
+                        mygpio.turnOn(mygpio.CONN_LIGHT_PIN) # Optional Conn LED
+                        clientRunning = True
+            time.sleep(.005) # Needed to catch the Keyboard Interrupt
+        
     except KeyboardInterrupt as e:
-        client.disconnect()
+        print("Detected Keyboard Interrupt")
         GPIO.cleanup()
+        client.disconnect()
         quit()
+
+
+
+if __name__ == "__main__":
+    main()
+
